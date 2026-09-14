bbeab3ac9c89913ebada9067737d120dd764c58f
YorichiiPrime <mohendragogoi66@gmail.com>
2026-09-14 23:29:21 +0530

    Revert "Update from task 7e36c404-7950-4e93-9955-6197228fba7b"

--- checker.py
+++ checker.py
@@ -571,8 +571,7 @@ class ProxyManager:
         self.log("Proxy pool cleared")
 
     def log(self, message, tag="PROXY"):
-        if self.ui_queue:
-            self.ui_queue.put(("log", tag, f"[{now_ts()}] {message}"))
+        self.ui_queue.put(("log", tag, f"[{now_ts()}] {message}"))
 
     def start(self):
         self.stop_event.clear()
